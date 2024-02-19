3f17b94aaef9711ea27a3670d57d4eed0d7d8090
fernando <fernandoalanfillmann@gmail.com>
2024-02-19 17:20:21 -0300

    diminuicao de delay

--- pages/cotacao/[id].tsx
+++ pages/cotacao/[id].tsx
@@ -23,7 +23,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps(context) {
-  await delay(12000);
+  await delay(5000);
   const id = context.params.id;
 
   return {
@@ -42,7 +42,6 @@ export default function Page(props) {
         justifyContent: "center"
       }}
     >
-      <h1>{props.id}</h1>
       {
         props.imgsSeguradoras.map((segImg) => 
           <div 
